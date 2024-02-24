01f836544899801eef265258f21da71a32aa06c2
merc07 <33531315+merc07@users.noreply.github.com>
2024-02-24 14:53:06 +0000

    Update version.c

--- version.c
+++ version.c
@@ -2,7 +2,8 @@
 #define __VER__          GIT_HASH
 
 //#define __VER_PREFIX__  "OEFW-"
-#define __VER_PREFIX__   "1o11-"
+//#define __VER_PREFIX__   "1o11-"
+#define __VER_PREFIX__   "Merc07"
 
 const char Version_str[] = __VER_PREFIX__ __VER__;
 #if defined(ENABLE_UART)
